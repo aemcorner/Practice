c5b4f7959a437fba585a5609af0313eca7f86a98
Vivek Dhiman <vivek.dhiman@ymail.com>
2016-09-09 10:00:35 -0700

    Adding Practice Component

--- core/src/main/java/com/vdhiman/aem/core/components/PracticeImpl.java
+++ core/src/main/java/com/vdhiman/aem/core/components/PracticeImpl.java
@@ -1,6 +1,12 @@
 package com.vdhiman.aem.core.components;
 
 import com.adobe.cq.sightly.WCMUse;
+import com.day.cq.wcm.api.Page;
+import org.apache.sling.api.resource.Resource;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * Created by vivek.dhiman on 08-09-2016.
@@ -8,20 +14,33 @@ import com.adobe.cq.sightly.WCMUse;
 public class PracticeImpl extends  WCMUse {
 
     private String pagePath;
-
-    private String heading;
+    private List<String> pageNames;
+    private List<String> createdBy;
 
     @Override
     public void activate() throws Exception {
-        heading = getProperties().get("heading","").toUpperCase();
-        pagePath = getCurrentPage().getPath();
+        pagePath = getProperties().get("pagepath","");
+        Resource rootPage = getResourceResolver().resolve(pagePath);
+        Page mypage = rootPage.adaptTo(Page.class);
+        Iterator<Page> iterator = mypage.listChildren();
+        pageNames = new ArrayList<String>();
+        createdBy = new ArrayList<String>();
+        while(iterator.hasNext()){
+           Page childPage = iterator.next();
+           pageNames.add(childPage.getName());
+           createdBy.add(childPage.getProperties().get("jcr:created").toString());
+        }
+        System.out.println("Size is ::: "+pageNames.size());
     }
-
     public String getPagePath() {
         return pagePath;
     }
-
-    public String getHeading() {
-        return heading;
+    public List<String> getPageNames() {
+        return pageNames;
     }
+    public List<String> getCreatedBy() {
+        return createdBy;
+    }
+
+
 }
